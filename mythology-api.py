5387bc930bb631f1d5c4a201868adb5faa200cf0
xabierlandaoregi <xabier.landa@alumni.mondragon.edu>
2020-10-29 11:49:32 +0100

    Initial commit

---
+++ mythology-api.py
@@ -0,0 +1,51 @@
+from flask import Flask
+from flask import jsonify
+from flask import Response
+from pymongo import MongoClient
+from bson.json_util import dumps
+
+mongoClient = MongoClient('mongodb+srv://zabi:zabi@cluster0.yvpfb.mongodb.net/greek-mythology?retryWrites=true&w=majority')
+db = mongoClient['greek-mythology']
+print(db.figures)
+
+app = Flask(__name__)
+
+
+@app.route('/figures',methods=["GET"])
+def figures():
+    cursor = db.figures.find()
+    listFig = list(cursor)
+    allFigures = dumps(listFig)
+    return Response(allFigures,mimetype="application/json")
+
+@app.route('/figures/<name>',methods=["GET"])
+def figure(name):
+    cursor = db.figures.find({"name":name})
+    listFig = list(cursor)
+    allFigures = dumps(listFig)
+    return Response(allFigures,mimetype="application/json")
+
+@app.route('/gods',methods=["GET"])
+def gods():
+    cursor = db.figures.find({"category":"major olympians"},{"name":1, "_id":0})
+    listFig = list(cursor)
+    allFigures = dumps(listFig)
+    return Response(allFigures,mimetype="application/json")
+
+@app.route('/titans',methods=["GET"])
+def titans():
+    cursor = db.figures.find({"category":"titan"},{"name":1, "_id":0})
+    listFig = list(cursor)
+    allFigures = dumps(listFig)
+    return Response(allFigures,mimetype="application/json")
+
+
+@app.route('/12titans',methods=["GET"])
+def titans12():
+    cursor = db.figures.find({"category":"twelve titan"},{"name":1, "_id":0})
+    listFig = list(cursor)
+    allFigures = dumps(listFig)
+    return Response(allFigures,mimetype="application/json")
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
